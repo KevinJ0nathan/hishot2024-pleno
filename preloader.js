bc6d53db2d681e1832dad7b8d37f180bb57c1fe8
Kevin Jonathan <142511694+KevinJ0nathan@users.noreply.github.com>
2024-05-29 18:32:41 +0700

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,16 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const preloader = document.getElementById('preloader');
-
-    // Ensure no-scroll class is applied initially
-    document.body.classList.add('no-scroll');
-
-    window.addEventListener('load', function() {
-        setTimeout(() => {
-            preloader.classList.add('fade-out');
-            preloader.addEventListener('transitionend', () => {
-                preloader.style.display = 'none';
-                document.body.classList.remove('no-scroll');
-            }, { once: true }); // Ensure the listener is only added once
-        }, 1000); // Delay for visual effect, adjust as needed
-    });
+    setTimeout(() => {
+        preloader.classList.add('fade-out');
+        preloader.addEventListener('transitionend', () => {
+            preloader.style.display = 'none';
+            document.body.classList.remove('no-scroll');
+        });
+    }, 6000); // 6000 milliseconds = 6 seconds
 });
